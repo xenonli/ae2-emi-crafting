091756f7051d22ef66b6eb4e7fdb7c7e09d02063
xenonli <126023994+xenonli@users.noreply.github.com>
2024-11-30 14:12:46 -0500

    Update RecipeUtils.java
    Fixing the recipe offset bug with AE2 crafting terminals

--- src/main/java/org/blocovermelho/ae2emicrafting/client/helper/RecipeUtils.java
+++ src/main/java/org/blocovermelho/ae2emicrafting/client/helper/RecipeUtils.java
@@ -6,7 +6,10 @@ import dev.emi.emi.api.recipe.VanillaEmiRecipeCategories;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.Recipe;
 import net.minecraft.recipe.ShapedRecipe;
+import net.minecraft.recipe.ShapelessRecipe;
 import net.minecraft.util.collection.DefaultedList;
+import org.blocovermelho.ae2emicrafting.client.Ae2EmiMod;
+import org.blocovermelho.ae2emicrafting.client.Ae2EmiPlugin;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -27,12 +30,32 @@ public class RecipeUtils {
             return true;
         }
     }
-
+    public static boolean canFit(ShapedRecipe recipe, int width, int height) {
+        DefaultedList<Ingredient> input = recipe.getIngredients();
+        if (input.size() > 9) {
+            return false;
+        }
+        for (int i = 0; i < input.size(); i++) {
+            int x = i % 3;
+            int y = i / 3;
+            if (!input.get(i).isEmpty() && (x >= width || y >= height)) {
+                return false;
+            }
+        }
+        return true;
+    }
     public static Map<Integer, Ingredient> getGuiSlotToIngredientMap(Recipe<?> recipe) {
         DefaultedList<Ingredient> ingredients = recipe.getIngredients();
         int width;
+        int sOff = 0;
         if (recipe instanceof ShapedRecipe shapedRecipe) {
             width = shapedRecipe.getWidth();
+            if(canFit(shapedRecipe, 1, 3)) {
+                sOff += 1;
+            }
+            if(canFit(shapedRecipe, 3, 1)) {
+                sOff += width;
+            }
         } else {
             width = 3;
         }
@@ -40,7 +63,7 @@ public class RecipeUtils {
         HashMap<Integer, Ingredient> result = new HashMap(ingredients.size());
 
         for(int i = 0; i < ingredients.size(); ++i) {
-            int guiSlot = i / width * 3 + i % width;
+            int guiSlot = i / width * 3 + i % width + sOff;
             Ingredient ingredient = (Ingredient)ingredients.get(i);
             if (!ingredient.isEmpty()) {
                 result.put(guiSlot, ingredient);
